86a899d28fec2a4af601d93dcae0fc75447e4119
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2019-02-09 23:21:09 +0530

    edited navbar

--- src/components/Layout/navbar.js
+++ src/components/Layout/navbar.js
@@ -5,7 +5,7 @@ const navbar = (props) => {
     return(
         <nav>
             <div className="nav-wrapper">
-                <Link to="/" className="brand-logo center"  style = {{"textDecoration": "none"}}>ReactVegaLite</Link>
+                <Link to="/" className="brand-logo center"  style = {{"textDecoration": "none"}}>React-Vega-Lite</Link>
             </div>
         </nav>
     );
