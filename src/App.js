c3c6a948e703244cedb6240d901b1b347fb81025
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2019-02-10 11:26:12 +0530

    added vegalite component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/Layout/navbar';
 import Dashboard from './containers/Dashboard/Dashboard';
+import Vegalite from './containers/vegalite/Vegalite';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <Dashboard />
+        <Vegalite />
       </div>
     );
   }
