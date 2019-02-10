9e39cf093466238723f11b710480d8548fbb4f03
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2019-02-10 21:58:08 +0530

    passed in errors

--- src/store/reducers/reducers.js
+++ src/store/reducers/reducers.js
@@ -12,11 +12,11 @@ const intialState = {
 const reducer = (state = intialState, action) => {
     switch(action.type){
         case (actionTypes.FETCH_START):
-            return updateObject(state, { loading: true, showgraph: false});
+            return updateObject(state, { loading: true, showgraph: false, error: ''});
         case (actionTypes.FETCH_SUCCESS):
-            return updateObject(state, { loading: false, data: Object.values(action["forecast"]), location: action.location, showgraph: true});
+            return updateObject(state, { loading: false, data: Object.values(action["forecast"]), location: action.location, showgraph: true, error: ''});
         case (actionTypes.FETCH_FAIL):
-            return updateObject(state, {loading: false, error: action.error, showgraph: false});
+            return updateObject(state, {loading: false, error: action.error, showgraph: false, data: ''});
         default:
             return state
     }
