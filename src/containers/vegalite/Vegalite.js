32382dd6ad25d4e1d77e8e12bb5e7f9cff25061d
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2019-02-10 11:43:28 +0530

    edited Vegalite.js

--- src/containers/vegalite/Vegalite.js
+++ src/containers/vegalite/Vegalite.js
@@ -2,28 +2,6 @@ import React, { Component } from 'react';
 import VegaLite from 'react-vega-lite';
 import { connect } from 'react-redux';
 
-// const spec = {
-//   "description": "A simple bar chart with embedded data.",
-//   "mark": "bar",
-//   "encoding": {
-//     "x": {"field": "a", "type": "ordinal"},
-//     "y": {"field": "b", "type": "quantitative"}
-//   }
-// };
-
-// const barData = {
-//   "values": [
-//     {"a": "A","b": 20}, {"a": "B","b": 34}, {"a": "C","b": 55},
-//     {"a": "D","b": 19}, {"a": "E","b": 40}, {"a": "F","b": 34},
-//     {"a": "G","b": 91}, {"a": "H","b": 78}, {"a": "I","b": 25}
-//   ]
-// };
-
-// ReactDOM.render(
-//   <VegaLite spec={spec} data={barData} />,
-//   document.getElementById('bar-container')
-// );
-
 class Vegalite extends Component{
   
   render(){
@@ -34,11 +12,9 @@ class Vegalite extends Component{
     }
     if(this.props.data.length){
       const data = this.props.data;
-      console.log(data);
       const spreadData = [
         ...data[0]
       ];
-      console.log(spreadData);
       barData = {
         "values": spreadData
       }
@@ -69,6 +45,7 @@ class Vegalite extends Component{
     return(
       <div className = "container center" style = {style}>
         <VegaLite spec={spec} data = {barData} />
+        <p className="center"><b>Weather forecast for {this.props.location}</b></p>
       </div>
     );
   }
@@ -77,7 +54,8 @@ class Vegalite extends Component{
 const mapStateToProps = state => {
   return{
     data: state.data,
-    showgraph: state.showgraph
+    showgraph: state.showgraph,
+    location: state.location
   };
 };
 
